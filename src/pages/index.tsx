749d4afa0eb8a316a1f2178890b9c5b813e26008
unknown <otavio.paiva93@yahoo.com.br>
2021-05-18 15:37:11 -0300

    Styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import styles from "../styles/home.module.scss";
 
-
-
 export default function Home() {
  return (
-   <div>
-     oi
-   </div>
+   <div className={styles.container}>
+     <div className={styles.board}>
+       <img src="" alt="Avatar" />
+       <input placeholder="Usuário"></input>
+       <input placeholder="Nome da sala"></input>
+       <input placeholder="Avatar"></input>
+       <button>Entrar</button>
+     </div>
+     </div>
  )
 }
