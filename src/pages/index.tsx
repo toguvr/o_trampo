c934eca1d02fe18f20d01de13a0ecb6b14bee433
Augusto <augustotf93@gmail.com>
2021-05-26 13:16:58 -0300

    fix logo inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,10 @@ export default function Home() {
       </Head>
       <div className={styles.board}>
         <img
-          src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png"
+          src={
+            values?.avatar ||
+            "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png"
+          }
           alt="Avatar"
         />
         <label>Avatar URL</label>
